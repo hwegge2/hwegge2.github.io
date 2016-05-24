0bffd031f2d4ba4bf9f1df56db60599dd7258abb
hwegge2 <poofasity@gmail.com>
2016-05-23 22:43:50 -0500

    Initial Site Setup

---
+++ js/test.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    // pop up .popover-test
+    $('.popover-test').popover();    
+        
+    // pop up #example1, #example2, #example3 with same content
+    $('[rel=popover]').popover({ 
+        html : true, 
+        content: function() {
+          return $('#popover_content_wrapper').html();
+        }
+    });
+
+});
+
